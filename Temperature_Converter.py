359be294e4f9b431e2379105f58065f0cfad92b7
kmb21 <113995857+kmb21@users.noreply.github.com>
2023-07-13 15:30:28 -0400

    Updated Temperature Converter

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -2,19 +2,23 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Temperature Converter")
-window.minsize(width=20,height=20)
+window.minsize(width=80,height=80)
+
+window.geometry("500x100")
 
 masterframe = tk.Frame(master=window,relief=tk.RAISED)
+masterframe.rowconfigure([0,1], weight=1, minsize=50)
+masterframe.columnconfigure([0,1,2,3], weight=1, minsize=50)
 def celciusToFahr():
     def get_Click1(event):
         txt = fld1.get()
         if txt != "":
             val = int(txt)
             fahr = (val * 9/5)+32
-            ans_lbl["text"] = str(fahr)
-    fld1 = tk.Entry(master=masterframe, width=10)
+            ans_lbl["text"] = str(fahr)+"°F"
+    fld1 = tk.Entry(master=masterframe)
     tempName = tk.Label(text="°C",master=masterframe)
-    ans_lbl = tk.Label(text="0°F",master=masterframe)
+    ans_lbl = tk.Label(text="32.0°F",master=masterframe)
     convert_bttn = tk.Button(master=masterframe, text="->", command=lambda: get_Click1(None))
     fld1.grid(row=0,column=0,sticky="ew")
     tempName.grid(row=0,column=1,stick="w")
@@ -30,10 +34,10 @@ def fahrToCelcius():
             cel = (val-32)*5/9
             fin_val["text"] = str(cel)
     pass
-    fld2 = tk.Entry(master=masterframe, width=10)
+    fld2 = tk.Entry(master=masterframe)
     tempName = tk.Label(master=masterframe, text="°F")
     btn = tk.Button(master=masterframe, text="->", command=lambda event: get_Click2(event))
-    fin_val = tk.Label(master=masterframe,text="32.0°F")
+    fin_val = tk.Label(master=masterframe,text=f"{-32*(5/9)}")
     fld2.grid(row=1,column=0,sticky="ew")
     tempName.grid(row=1,column=1,stick="w")
     btn.grid(row=1,column=2,padx=5, sticky="nesw")
@@ -43,7 +47,7 @@ def fahrToCelcius():
 
 
 celciusToFahr()
-fahrToCelcius
+fahrToCelcius()
 
 masterframe.pack(expand=True,fill=tk.BOTH)
 
